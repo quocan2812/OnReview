1d25b6c8361a7b1bfdce450cf1b16277f422d054
AN <hoang.quoc.an@gmail.com>
2021-05-22 12:03:22 +0700

    [will Set and Did Set]

--- Swift_Review/ViewController.swift
+++ Swift_Review/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 class ViewController: UIViewController {
     
     let mainViewModel = MainViewModel()
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // setup some closure model
+        
+        self.mainViewModel.showAlertWithMessageAndTitle = { (message : String, title : String) -> () in
+            
+            self.showAlert(title: message, message: title)
+        }
+        
+        self.mainViewModel.disSetValuePlayGirl = {(numberPlayGirl : Int) -> () in
+            self.inputTextField.text = "\(numberPlayGirl)"
+        }
+        
+        
+        
+        
 
         
         // Do any additional setup after loading the view.
@@ -40,6 +58,30 @@ class ViewController: UIViewController {
         
           
     }
+    
+    
+    @IBAction func onTouchConfirmButton(_ sender: UIButton) {
+        
+        guard let numberGirl = inputTextField.text else {
+            return
+        }
+        
+        self.mainViewModel.numberOfPlayGirl = Int(numberGirl) ?? 0
+        
+        
+    }
+    
+    func showAlert(title: String, message : String){
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
     
 }
 
--- Swift_Review/ViewModel/MainViewModal.swift
+++ Swift_Review/ViewModel/MainViewModal.swift
@@ -11,6 +11,32 @@ class MainViewModel: NSObject {
     
     var listArticals : [Artical]
     
+    var numberOfPlayGirl : Int = 0 {
+        
+        willSet {
+            if newValue < 1000 {
+                showAlertWithMessageAndTitle("Oh master quốc chơi nhiều hơn bạn nghĩ","Sai rồi bạn tôi!!")
+            }
+        }
+        
+        didSet {
+            if numberOfPlayGirl >= 1000 {
+                showAlertWithMessageAndTitle("Oh year, master Quốc thật sung sức","Well done!!")
+                self.disSetValuePlayGirl(numberOfPlayGirl)
+            }
+            
+        }
+    }
+    
+    var showAlertWithMessageAndTitle :(String, String) -> () = { _,_  in }
+    
+    var disSetValuePlayGirl :(Int) -> () = {_ in }
+    
+    
+    override init() {
+        self.listArticals = []
+    }
+    
     func callDemoApi() {
         Network.shared().callApiDemo { (sucess, resultArtical) in
             
@@ -21,8 +47,7 @@ class MainViewModel: NSObject {
             print("\(self.listArticals[0])")
         }
     }
-    override init() {
-        self.listArticals = []
-    }
+    
+  
 
 }
